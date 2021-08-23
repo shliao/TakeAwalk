60e32b016618f1c68d50aa56459c0cb177d9a63c
A6228774 <A6228774@ulive.pccu.edu.tw>
2021-08-23 14:42:21 +0900

    Default頁面抓取資料庫 新增TicketManager

---
+++ TakeAwalk/TakeAwalk.DBSource/TicketManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TakeAwalk.ORM.DBModels;
+
+namespace TakeAwalk.DBSource
+{
+    public class TicketManager
+    {
+        public static List<TrainTicket> GetTrainTicketsList_Default()
+        {
+            using (ContextModel context = new ContextModel())
+            {
+                try
+                {
+                    var query = (from item in context.TrainTickets
+                                 select item);
+
+                    var list = query.ToList();
+                    return list;
+                }
+                catch (Exception ex)
+                {
+                    Logger.WriteLog(ex);
+                    return null;
+                }
+            }
+        }
+    }
+}
--- TakeAwalk/TakeAwalk/Default.aspx.cs
+++ TakeAwalk/TakeAwalk/Default.aspx.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using TakeAwalk.DBSource;
 namespace TakeAwalk
 {
     public partial class Default : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            this.gdvTicket.DataSource = TicketManager.GetTrainTicketsList_Default();
+            this.gdvTicket.DataBind();
         }
     }
 }
