24a618251469a0f8f856bea6e4bcf6368ac2c468
A6228774 <A6228774@ulive.pccu.edu.tw>
2021-08-24 17:28:30 +0900

    ．修改主頁為Orderby存庫量

--- TakeAwalk/TakeAwalk.DBSource/TicketManager.cs
+++ TakeAwalk/TakeAwalk.DBSource/TicketManager.cs
@@ -28,5 +28,26 @@ namespace TakeAwalk.DBSource
                 }
             }
         }
+        public static List<TrainTicket> GetTrainTicketsList_OrderByInventory()
+        {
+            using (ContextModel context = new ContextModel())
+            {
+                try
+                {
+                    var query = (from item in context.TrainTickets
+                                 orderby item.Inventory descending
+                                 select item);
+
+                    var list = query.ToList();
+                    return list;
+                }
+                catch (Exception ex)
+                {
+                    Logger.WriteLog(ex);
+                    return null;
+                }
+            }
+        }
+
     }
 }
