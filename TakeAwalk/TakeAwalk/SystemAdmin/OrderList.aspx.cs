89e5b2b79b4f8e3ed3b58f4423114ca28926aa09
A6228774 <A6228774@ulive.pccu.edu.tw>
2021-08-27 09:14:41 +0900

    ．修正管理者登入從客戶資料跳頁顯示不了該客戶訂單的問題

--- TakeAwalk/TakeAwalk/SystemAdmin/OrderList.aspx.cs
+++ TakeAwalk/TakeAwalk/SystemAdmin/OrderList.aspx.cs
@@ -15,21 +15,25 @@ namespace TakeAwalk.SystemAdmin
         {
             var currentUser = AuthManager.GetCurrentUser();
 
+            if (!IsPostBack)
+            {
+                this.gv_orderlist.DataSource = OrdersManager.GetOrdersListbyCustomerID(currentUser.CustomerID);
+                this.gv_orderlist.DataBind();
+            }
+
             if (currentUser.UserLevel == 0)
             {
                 string guidtxt = this.Request.QueryString["CustomerID"];
+                this.gv_orderlist.DataSource = OrdersManager.GetOrdersListbyCustomerID(currentUser.CustomerID);
+                this.gv_orderlist.DataBind();
 
-                if (guidtxt == string.Empty)
+                if (guidtxt != null)
                 {
                     Guid customerid = Guid.Parse(guidtxt);
                     this.gv_orderlist.DataSource = OrdersManager.GetOrdersListbyCustomerID(customerid);
                     this.gv_orderlist.DataBind();
                 }
-
-                this.gv_orderlist.DataSource = OrdersManager.GetOrdersListbyCustomerID(currentUser.CustomerID);
             }
-            this.gv_orderlist.DataSource = OrdersManager.GetOrdersListbyCustomerID(currentUser.CustomerID);
-            this.gv_orderlist.DataBind();
         }
     }
 }
